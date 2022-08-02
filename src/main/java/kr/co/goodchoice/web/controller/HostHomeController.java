4bfed98ce0398e107f49b6b4d1b12f8993d59083
soyeon <dlathdus159@naver.com>
2022-08-03 00:44:24 +0900

    hosthome 수정 및 실시간 예약, 공지사항, 자주 묻는 질문 구현

--- src/main/java/kr/co/goodchoice/web/controller/HostHomeController.java
+++ src/main/java/kr/co/goodchoice/web/controller/HostHomeController.java
@@ -6,8 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HostHomeController {
 
+	// 호스트 홈
 	@GetMapping(path = "/hosthome")
 	public String hosthome() {
 		return "host/hosthome";
+	}
+	
+	// 호스트 공지사항
+	@GetMapping(path = "/hostnotice")
+	public String hostnotice() {
+		return "host/hostnoticelist";
+	}
+	
+	// 호스트 자주묻는 질문
+	@GetMapping(path = "/hostquestion")
+	public String hostquestion() {
+		return "host/oftenquestionlist";
+	}
+	
+	// 실시간 예약 관리
+	@GetMapping(path = "/realtimereserve")
+	public String realtimereserve( ) {
+		return  "host/realtimereserve";
 	}
 }
