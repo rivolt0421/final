83057b97524ddd611eb7035a21044c0eff3fa883
soyeon <dlathdus159@naver.com>
2022-08-17 04:10:19 +0900

    Often 수정

--- src/main/java/kr/co/goodchoice/vo/OftenCategory.java
+++ src/main/java/kr/co/goodchoice/vo/OftenCategory.java
@@ -1,12 +1,17 @@
 package kr.co.goodchoice.vo;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
+@Alias("OftenCategory")
 public class OftenCategory {
 
 	private String id;
