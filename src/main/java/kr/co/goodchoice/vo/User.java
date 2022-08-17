60b9ae656c1134394c091136a952b730d7d416a4
kosarbang <kimjeongchan908@gmail.com>
2022-08-17 18:14:27 +0900

    myinfo 수정구현

--- src/main/java/kr/co/goodchoice/vo/User.java
+++ src/main/java/kr/co/goodchoice/vo/User.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import org.apache.ibatis.type.Alias;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,16 +22,27 @@ import lombok.ToString;
 @Alias("User")
 public class User {
 
+	@JsonIgnore
 	private int no;
+	@JsonIgnore
 	private String name;
+	@JsonIgnore
 	private String email;
+	@JsonIgnore
 	private String password;
+	@JsonIgnore
 	private String tel;
+	@JsonIgnore
 	private String nickname;
+	@JsonIgnore
 	private int point;
+	@JsonIgnore
 	private Date createdDate;
+	@JsonIgnore
 	private Date updatedDate;
+	@JsonIgnore
 	private String disabled;
+	@JsonIgnore
 	private String host;
 	
 }
