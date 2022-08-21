5b3b49dd359a293b2f6d94883da28975be63c861
inkook <kik1112@naver.com>
2022-08-22 08:11:23 +0900

    예약내역 페이지 구현중입니다.

--- src/main/java/kr/co/goodchoice/mapper/ReservationMapper.java
+++ src/main/java/kr/co/goodchoice/mapper/ReservationMapper.java
@@ -1,5 +1,18 @@
 package kr.co.goodchoice.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.goodchoice.vo.Reservation;
+
+@Mapper
 public interface ReservationMapper {
+
+		// reservation 리스트
+		List<Reservation> getReservations();
+		// reservationList userId로 
+		Reservation getReservationByUserId(String id);
+	
 
 }
